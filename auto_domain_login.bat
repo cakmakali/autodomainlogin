cf1138aa081c9d9918fe8c4435ac937b3786426b
ali çakmak <76594548+cakmakali@users.noreply.github.com>
2021-07-16 21:34:37 +0300

    Update auto_domain_login.bat

--- auto_domain_login.bat
+++ auto_domain_login.bat
@@ -1,5 +1,5 @@
 @echo off
-::only change your-dnsip-for-domain,your-domain-here,your-domain-pass
-for /f "skip=3 tokens=3*" %%a in ('netsh int show int') do netsh int ip set dns name="%%b" static your-dnsip-for-domain
-powershell.exe netdom join %computername% /domain:your-domain-here /ud:%computername% /pd:your-domain-pass
+::only change dnsip-for-domain,domain-name,domain-pass
+for /f "skip=3 tokens=3*" %%a in ('netsh int show int') do netsh int ip set dns name="%%b" static dnsip-for-domain
+powershell.exe netdom join %computername% /domain:domain-name /ud:%computername% /pd:domain-pass
 pause
